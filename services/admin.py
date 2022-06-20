0edd6cbe0dc26adc5d8812d0e26cdec029401e4a
ollylomax <ollylomax@yahoo.com>
2022-06-20 13:05:57 +0000

    Sort by fields in admin.py, set up views and urls

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Category, Service
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'paper_size',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
 
 # Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Service, ServiceAdmin)
--- services/models.py
+++ services/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -27,7 +31,6 @@ class Service(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     paper_size = models.CharField(
         max_length=2,
