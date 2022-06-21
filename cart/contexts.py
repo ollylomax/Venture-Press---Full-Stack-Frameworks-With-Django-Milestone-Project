67285ff712077ca828b04779a98a86e7a1efe3e0
ollylomax <ollylomax@yahoo.com>
2022-06-21 10:11:05 +0000

    Iterate through cart items in contexts.py to render to template

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from services.models import Service
 
 
 def cart_contents(request):
@@ -7,6 +9,18 @@ def cart_contents(request):
     cart_items = []
     total = 0
     service_count = 0
+
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        service = get_object_or_404(Service, pk=item_id)
+        total += quantity * service.price
+        service_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'service': service,
+        })
 
     if total < settings.DELIVERY_THRESHOLD:
         delivery = (total + settings.DELIVERY_CHARGE) * Decimal(settings.DELIVERY_PERCENT / 100)
