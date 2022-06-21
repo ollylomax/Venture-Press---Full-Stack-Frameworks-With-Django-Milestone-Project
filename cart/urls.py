599e8d88760c2e9342336583f009d2a67b7e273a
ollylomax <ollylomax@yahoo.com>
2022-06-21 14:55:24 +0000

    Add edit quantity and delete views and urls

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('amend/<item_id>/', views.quantity_amend, name='quantity_amend'),
+    path('delete/<item_id>/', views.delete_from_cart, name='delete_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from services.models import Service
 
 # Create your views here.
 
@@ -12,14 +15,53 @@ def cart(request):
 def add_to_cart(request, item_id):
     """ View to add quantity of chosen product to cart """
 
+    service = Service.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     path = request.POST.get('path')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {service.name} quantity to {cart[item_id]}x')
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {quantity}x {service.name} to your bag')
 
     request.session['cart'] = cart
     return redirect(path)
+
+
+def quantity_amend(request, item_id):
+    """Amend the quantity of the chosen product """
+
+    quantity = int(request.POST.get('quantity'))
+    service = Service.objects.get(pk=item_id)
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+        messages.success(request, f'Updated {service.name} quantity to {cart[item_id]}x')
+    else:
+        cart.pop(item_id)
+        messages.success(request, f'Removed all {service.name} from your bag')
+
+    request.session['cart'] = cart
+    return redirect(reverse('cart'))
+
+
+def delete_from_cart(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    cart = request.session.get('cart', {})
+    service = Service.objects.get(pk=item_id)
+    
+    try:
+        cart.pop(item_id)
+        messages.success(request, f'Removed all {service.name} from your bag')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error when removing item: {e}')
+        return HttpResponse(status=500)
