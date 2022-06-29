4bc73a2db2694a236298f32dc741e99aadbaa13a
ollylomax <ollylomax@yahoo.com>
2022-06-29 09:34:43 +0000

    Link stripe keys to environment variables and add imports in views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,42 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from cart.contexts import cart_contents
+from .models import Order, OrderLineItem
+from services.models import Service
+
+import stripe
 
 
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Nothing in your bag")
         return redirect(reverse('services'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51L8M0hHB8OpqVDbT2n0RMtNLfwET8tHCksJXt6QzFgTEHHLl80jOmGRPuH532pMSuyOGHjOt5iFAEVWEmArTLCtW00FrPlxH7n',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
