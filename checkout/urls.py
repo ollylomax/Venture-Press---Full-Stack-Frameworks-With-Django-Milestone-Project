6225fcd5e703ca65ad546adc862c0272faabe4e3
ollylomax <ollylomax@yahoo.com>
2022-06-29 09:42:41 +0000

    Wire up checked out view to new template to be created in urls.py

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('checked_out/<order_number>', views.checked_out, name='checked_out'),
 ]
