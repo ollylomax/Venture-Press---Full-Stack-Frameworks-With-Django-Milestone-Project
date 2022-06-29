0bef4529dc5d9d4b30afa84ff417fb866dbc65c8
ollylomax <ollylomax@yahoo.com>
2022-06-29 15:02:41 +0000

    Fix: adjust models to reflect base delivery charge

--- checkout/models.py
+++ checkout/models.py
@@ -33,9 +33,9 @@ class Order(models.Model):
         """
         Each time a line item is added, update grand total with delivery costs
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
-        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.DELIVERY_PERCENT / 100
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total < settings.DELIVERY_THRESHOLD:
+            self.delivery_cost = settings.DELIVERY_CHARGE + (self.order_total * settings.DELIVERY_PERCENT) / 100
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
