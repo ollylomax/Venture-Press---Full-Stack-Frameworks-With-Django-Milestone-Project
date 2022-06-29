eff896453fa5b98aa983be6ec94bb360ffbe24bd
ollylomax <ollylomax@yahoo.com>
2022-06-29 09:36:32 +0000

    Add POST conditional to checkout view for form capture

--- checkout/views.py
+++ checkout/views.py
@@ -15,22 +15,62 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "Nothing in your bag")
-        return redirect(reverse('services'))
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
 
-    current_cart = cart_contents(request)
-    total = current_cart['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'address_line1': request.POST['street_address1'],
+            'address_line2': request.POST['street_address2'],
+            'city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+            'postcode': request.POST['postcode'],
+            'country': request.POST['country'],
+            'phone_number': request.POST['phone_number'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, quantity in cart.items():
+                try:
+                    service = get_object_or_404(Service, pk=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        service=service,
+                        quantity=quantity,
+                    )
+                    order_line_item.save()
 
-    order_form = OrderForm()
+                except Service.DoesNotExist:
+                    messages.error(request, (
+                        "Service Not Found")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checked_out', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "Nothing in your bag")
+            return redirect(reverse('services'))
+
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     template = 'checkout/checkout.html'
     context = {
