974b45db92a048b7afc6a927e3fd023dc13b92d8
ollylomax <ollylomax@yahoo.com>
2022-06-30 20:40:06 +0000

    Add post method to profile view, create order history view and wire up url paths

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,42 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Profile page view """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'accounts/profile.html'
     context = {
+        'form': form,
+        'orders': orders,
+        'profile': profile,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request):
+    """ Order History page view """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    orders = profile.orders.all()
+
+    template = 'accounts/order_history.html'
+    context = {
+        'orders': orders,
         'profile': profile,
     }
 
