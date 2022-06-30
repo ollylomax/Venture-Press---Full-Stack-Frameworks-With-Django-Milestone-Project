5088674fcc72299cf148f6d76876cb287a9b1599
ollylomax <ollylomax@yahoo.com>
2022-06-30 07:56:41 +0000

    Add url path for checkout cache view

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,6 +4,7 @@ from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
+    path('checkout_cache/', views.checkout_cache, name='checkout_cache'),
     path('checked_out/<order_number>/', views.checked_out, name='checked_out'),
     path('webhook/', webhook, name='webhook'),
 ]
