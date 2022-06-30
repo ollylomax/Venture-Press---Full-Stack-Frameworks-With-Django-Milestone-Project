1159675d35ab3492679651129516445685cdd097
ollylomax <ollylomax@yahoo.com>
2022-06-30 20:35:05 +0000

    Add profile and order history links to base.html and wire up url paths

--- venture_press/urls.py
+++ venture_press/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('services/', include('services.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
-    path('profile/', include('accounts.urls')),
+    path('useraccounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
