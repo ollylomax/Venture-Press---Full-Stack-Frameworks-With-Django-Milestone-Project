e42e5cd6d71fbeca815a376bed416b80700b7c9a
ollylomax <ollylomax@yahoo.com>
2022-07-01 17:36:08 +0000

    fix: Adjust mini cart fields to yield correct delivery information and fix typo in views.py accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from checkout.models import Order
