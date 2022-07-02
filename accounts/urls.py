f628851f1fdd3c5c8f341d99cb324d1c6620a63e
ollylomax <ollylomax@yahoo.com>
2022-07-02 21:25:44 +0000

    Wire up url paths for message centre and past messages, create their templates and link from profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('orders/', views.order_history, name='order_history'),
-    path('past_order/<order_number>', views.past_order, name='past_order'),
+    path('past_order/<order_number>/', views.past_order, name='past_order'),
+    path('messages/', views.message_centre, name='message_centre'),
+    path('past_message/<message_id>/', views.past_message, name='past_message'),
 ]
