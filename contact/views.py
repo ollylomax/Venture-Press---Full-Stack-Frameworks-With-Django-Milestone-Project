6950b98113594cd151ec0a88771463c3094d86c8
ollylomax <ollylomax@yahoo.com>
2022-07-02 17:39:11 +0000

    Set up django mass send for sending confirmation email to user and also email to venture press mail

--- contact/views.py
+++ contact/views.py
@@ -3,7 +3,7 @@ from .forms import ContactForm
 from django.http import HttpResponse
 from django.contrib import messages
 from django.template.loader import render_to_string
-from django.core.mail import send_mail
+from django.core.mail import send_mass_mail
 
 from django.conf import settings
 
@@ -23,13 +23,22 @@ def contact(request):
         if form.is_valid():
             form.save()
             subject = render_to_string('contact/contact_email/contact_email_subject.txt', {'form': form})
-            body = render_to_string('contact/contact_email/contact_email_body.txt', {'form': form})
-            send_mail(
+            customer_email_body = render_to_string('contact/contact_email/contact_confirmation_email_body.txt', {'form': form})
+            company_email_body = render_to_string('contact/contact_email/contact_from_email_body.txt', {'form': form})
+            customer_email = (
                 subject,
-                body,
-                form['email'].value(),
+                customer_email_body,
+                settings.VENTURE_PRESS_EMAIL,
+                [form['email'].value()]
+            )
+            company_email = (
+                subject,
+                company_email_body,
+                settings.VENTURE_PRESS_EMAIL,
                 [settings.VENTURE_PRESS_EMAIL]
             )
+            send_mass_mail((customer_email, company_email), fail_silently=False)
+            print(form['email'].value())
             return redirect('success')
  
     return render(request, "contact/contact.html", {'form': form})
