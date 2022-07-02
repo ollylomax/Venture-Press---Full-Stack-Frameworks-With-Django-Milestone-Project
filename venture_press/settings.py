3ef977c3da47abb47bb807ba7c0f35225d7d62b7
ollylomax <ollylomax@yahoo.com>
2022-07-02 18:58:39 +0000

    Modify venture press email in settings.py

--- venture_press/settings.py
+++ venture_press/settings.py
@@ -210,7 +210,7 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    VENTURE_PRESS_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # AWS S3
 
