2f7a5b5285001b6bdde7db6e96acb7e6bf3532b6
ollylomax <ollylomax@yahoo.com>
2022-07-02 14:47:06 +0000

    Build contact and success views to send emails, and update model fields in admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Messages
 
-# Register your models here.
+
+class MessagesAdmin(admin.ModelAdmin):
+
+    fields = ('first_name', 'last_name', 'email', 'subject', 'message')
+
+    list_display = ('first_name', 'last_name', 'email', 'subject')
+
+
+admin.site.register(Messages, MessagesAdmin)
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.http import HttpResponse
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
 
-# Create your views here.
+from django.conf import settings
+
+
+def contact(request):
+    if request.method == 'GET':
+        form = ContactForm()
+        if request.user.username:
+            form['first_name'].initial = request.user.first_name
+            form['last_name'].initial = request.user.last_name
+            form['email'].initial = request.user.email
+
+    else:
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            subject = render_to_string('contact/contact_email/contact_email_subject.txt', {'form': form})
+            body = render_to_string('contact/contact_email/contact_email_body.txt', {'form': form})
+            send_mail(
+                subject,
+                body,
+                form['email'].value(),
+                [settings.VENTURE_PRESS_EMAIL]
+            )
+            return redirect('success')
+ 
+    return render(request, "contact/contact.html", {'form': form})
+
+
+def success(request):
+    messages.success(request, "Your contact request has been received")
+    return render(request, "contact/success.html")
