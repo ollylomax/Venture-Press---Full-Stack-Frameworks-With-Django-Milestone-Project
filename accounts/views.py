6f849189021b5a12a9f51aa64ff2076286dcb2f1
ollylomax <ollylomax@yahoo.com>
2022-07-02 21:23:55 +0000

    Build message centre and past messages views in profile app

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from checkout.models import Order
 from .models import UserProfile
 from .forms import UserProfileForm
+from contact.models import Messages
 
 
 @login_required
 def profile(request):
     """ Profile page view """
     profile = get_object_or_404(UserProfile, user=request.user)
-
+    user_messages = get_list_or_404(Messages, user=request.user)
+    
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
@@ -29,6 +31,7 @@ def profile(request):
         'form': form,
         'orders': orders,
         'profile': profile,
+        'user_messages': user_messages,
     }
 
     return render(request, template, context)
@@ -60,3 +63,34 @@ def past_order(request, order_number):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def message_centre(request):
+    """ Message centre page view """
+
+    user_messages = get_list_or_404(Messages, user=request.user)
+
+    template = 'accounts/messages.html'
+    context = {
+        'user_messages': user_messages,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def past_message(request, message_id):
+    message = get_object_or_404(Messages, pk=message_id)
+
+    if request.user == message.user:
+
+        template = 'accounts/past_message.html'
+        context = {
+            'message': message,
+        }
+
+        return render(request, template, context)
+    else:
+        messages.error(request, 'Invalid Message')
+        return redirect(message_centre)
