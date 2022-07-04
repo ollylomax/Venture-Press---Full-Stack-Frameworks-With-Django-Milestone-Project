dc16543ee5276a146d298ebe2c66e72d64983f4b
ollylomax <ollylomax@yahoo.com>
2022-07-04 17:40:01 +0000

    fix: Fix issue with orders template loop and tidy html

--- artwork/views.py
+++ artwork/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404, reverse
 from django.contrib import messages
 
 from checkout.models import Order
+from checkout.forms import OrderForm
 from accounts.models import UserProfile
 
 from .models import Artwork
@@ -12,6 +13,7 @@ def orders_requiring_artwork(request):
 
     # Init empty form
     form = ArtworkUpload()
+    order_form = OrderForm()
     
     order_filter = get_object_or_404(UserProfile, user=request.user)
     user_orders = order_filter.orders.all()
@@ -21,41 +23,51 @@ def orders_requiring_artwork(request):
 
     for order in orders:  
         form['order'].initial = order
-        print(order)
 
     # File urls rendered to template
     files = Artwork.objects.all()
 
     # Render the artwork template with context
-    context = {'files': files, 'form': form, 'orders': orders}
+    context = {
+        'files': files,
+        'form': form,
+        'orders': orders,
+        'order_form': order_form,
+        }
+
     return render(request, "artwork/artwork.html", context)
 
 
 def upload_artwork(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+    order_form = OrderForm(instance=order)
 
      # Handle file upload
     if request.method == 'POST':
         files = Artwork.objects.all()
         form = ArtworkUpload(request.POST, request.FILES)
-        user = get_object_or_404(UserProfile, user=request.user)
-        order = get_object_or_404(Order, order_number=order_number)
+        form['order'].initial = order
 
-        form_data = {
-            'user': user,
-            'order': order,
-            'upload': request.FILES['upload'],
-        }
+        if form.is_valid():           
+            artworks = form.save()
+            artworks.order = order
+            artworks.save()
+            update_order = order_form.save(commit=False)
+            update_order.has_artwork = True
+            update_order.save()
+            messages.success(request, 'Your Artwork has been Successfully Received')
 
-        cust_artwork = ArtworkUpload(form_data)
-        print(cust_artwork)
-        if form.is_valid():
-            order.has_artwork = True
-            form.save()
+            return redirect(reverse('orders_requiring_artwork'))
+
         else:
-            messages.error(request, 'NOPE')
-            # print(form.errors.as_data())
+            messages.error(request, 'Issue when Uploading Artwork')
+            print(form.errors.as_data())
+        
+        context = {
+            'files': files,
+            'form': form,
+            'order': order,
+            'order_form': order_form}
 
-            # return redirect('orders_requiring_artwork')
-
-        context = {'files': files, 'form': form}
         return render(request, "artwork/artwork.html", context)
+
