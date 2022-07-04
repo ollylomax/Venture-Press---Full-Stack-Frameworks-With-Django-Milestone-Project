9918b151efebd59603e68523bec5a83d47c6b977
ollylomax <ollylomax@yahoo.com>
2022-07-04 17:39:00 +0000

    Adjust fields in checkout admin to display artwork upload

--- checkout/admin.py
+++ checkout/admin.py
@@ -24,7 +24,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
-                    'grand_total',)
+                    'grand_total', 'has_artwork')
 
     ordering = ('-date',)
 
