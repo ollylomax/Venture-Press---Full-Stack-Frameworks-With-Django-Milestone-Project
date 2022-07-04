b1942156224d541f660cd596c21ac05b87fe6e37
ollylomax <ollylomax@yahoo.com>
2022-07-04 17:37:55 +0000

    Build file directory function in artwork models

--- artwork/models.py
+++ artwork/models.py
@@ -4,7 +4,11 @@ from accounts.models import UserProfile
 from checkout.models import Order
 
 
+def artwork_directory(instance, filename):
+    return 'artwork/order_{0}/{1}'.format(instance.order, filename)
+
+
 class Artwork(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    upload = models.FileField(upload_to='artwork/%d/%m/%Y')
+    upload = models.FileField(upload_to=artwork_directory)
