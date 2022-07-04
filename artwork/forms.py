b4367a3f144514eaec08f91aa0a8a00c9c61b0f8
ollylomax <ollylomax@yahoo.com>
2022-07-04 17:37:01 +0000

    Add hidden fields to checkout forms

--- artwork/forms.py
+++ artwork/forms.py
@@ -5,6 +5,10 @@ from .models import Artwork
 class ArtworkUpload(forms.ModelForm):
     class Meta:
         model = Artwork
+        widgets = {
+            'user': forms.HiddenInput(),
+            'order': forms.HiddenInput(),
+        }
         fields = ('user', 'order', 'upload')
         
     upload = forms.FileField(label='Upload Artwork')
