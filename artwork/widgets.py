82e1041b4918f899b6301fd29b2d65d5f4ca8db2
ollylomax <ollylomax@yahoo.com>
2022-07-05 22:56:21 +0000

    Build custom widget for clearable file input for artwork file upload table

---
+++ artwork/widgets.py
@@ -0,0 +1,6 @@
+from django.forms.widgets import ClearableFileInput
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomClearableFileInput(ClearableFileInput):
+    template_name = 'artwork/custom_widget_templates/custom_clearable_file_input.html'
