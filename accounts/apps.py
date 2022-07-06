225f816843474798fe01654b9c02653762c6db88
ollylomax <ollylomax@yahoo.com>
2022-07-06 13:45:00 +0000

    chore: Add missing docstrings and tidy existing comments in accounts apps and views

--- accounts/apps.py
+++ accounts/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    """
+    Load accounts configuration.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
+from django.shortcuts import (
+    render,
+    get_object_or_404,
+    get_list_or_404,
+    redirect,
+)
+
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -9,9 +15,12 @@ from .forms import UserProfileForm
 from contact.models import Messages
 
 
+# Require session decorator from django decorators.
 @login_required
 def profile(request):
-    """ Profile page view """
+    """ Profile page view matching current user session with user profile and
+    passing it to the profile template """
+
     profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'accounts/profile.html'
@@ -23,9 +32,15 @@ def profile(request):
     return render(request, template, context)
 
 
+# Require session decorator from django decorators.
 @login_required
 def edit_profile(request):
-    """ Update Profile page view """
+    """ View to update user profile by matching current user session with user
+    profile, instantiating UserProfile form with current profile then updating
+    on POST. Get associated User object and update first and last name to match
+    profile. If GET request then instantiate UserProfile form with current
+    profile."""
+
     profile = get_object_or_404(UserProfile, user=request.user)
     
     if request.method == 'POST':
@@ -42,22 +57,22 @@ def edit_profile(request):
     else:
         form = UserProfileForm(instance=profile)
 
-    orders = profile.orders.all()
-
     template = 'accounts/edit_profile.html'
     context = {
         'form': form,
-        'orders': orders,
         'profile': profile,
     }
 
     return render(request, template, context)
 
 
-
+# Require session decorator from django decorators.
 @login_required
 def order_history(request):
-    """ Order History page view """
+    """ View to display Order History by matching current session with
+    associated profile, setting a variable for all associated orders and
+    passing both profile and orders to the template."""
+
     profile = get_object_or_404(UserProfile, user=request.user)
 
     orders = profile.orders.all()
@@ -71,8 +86,13 @@ def order_history(request):
     return render(request, template, context)
 
 
+# Require session decorator from django decorators.
 @login_required
 def past_order(request, order_number):
+    """ View to display a previous user order by passing order number from the
+    template, get the order object by order number, save it to a variable then
+    pass it back to the template."""
+
     order = get_object_or_404(Order, order_number=order_number)
 
     template = 'accounts/past_order.html'
@@ -83,9 +103,13 @@ def past_order(request, order_number):
     return render(request, template, context)
 
 
+# Require session decorator from django decorators.
 @login_required
 def message_centre(request):
-    """ Message centre page view """
+    """ View to display previously submitted user contact messages by checking
+    if there are messages in the Message object filtered by current user
+    session. Conditionally save filtered list of objects to a variable if
+    messages exist then pass it to template."""
     
     if not Messages.objects.filter(user=request.user).exists():
         user_messages = None
@@ -100,8 +124,13 @@ def message_centre(request):
     return render(request, template, context)
 
 
+# Require session decorator from django decorators.
 @login_required
 def past_message(request, message_id):
+    """ View to display a previously submitted user contact message by passing
+    message id from the template, get the message object by message id and
+    save it to a variable. Check if the user from user session matches the user
+    in message variable, and if so pass it back to the template."""
     message = get_object_or_404(Messages, pk=message_id)
 
     if request.user == message.user:
